39458763d9c2b24b5cc2b56ef3fe0b928c0f2b89
Tarjana <104441249+Tarjana@users.noreply.github.com>
2022-04-27 13:31:12 -0400

    Update SampleClient.java

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -1,6 +1,11 @@
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+import ca.uhn.fhir.util.BundleUtil;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
 
@@ -14,13 +19,26 @@ public class SampleClient {
         client.registerInterceptor(new LoggingInterceptor(false));
 
         // Search for Patient resources
+        
+        List<IBaseResource> patients = new ArrayList<>();
         Bundle response = client
                 .search()
                 .forResource("Patient")
                 .where(Patient.FAMILY.matches().value("SMITH"))
                 .returnBundle(Bundle.class)
                 .execute();
+        
+        patients.addAll(BundleUtil.toListOfResources(fhirContext, response));
+             System.out.println("Loaded " + patients.size() + " patients!");
 
+             Collections.sort(patients, (p1, p2) -> {
+                return ((Patient) p1).getNameFirstRep().getGivenAsSingleString().compareToIgnoreCase(((Patient) p2).getNameFirstRep().getGivenAsSingleString()); 
+             });
+
+             patients.forEach(p -> {
+                System.out.print("Patient first name and last name:"+((Patient) p).getNameFirstRep().getNameAsSingleString());
+                System.out.println(", birth date:"+String.format("%1$tb %1$te, %1$tY", ((Patient) p).getBirthDate()));
+             });
 
     }
 
